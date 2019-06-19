f0bd5d75e1aa308470c0fba076b4643e67a78778
Christopher Payne <christopher.payne@globalsign.com>
2019-06-19 19:27:51 +0900

    Fixing stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-=======
   before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -9,5 +7,4 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
->>>>>>> modeling-users
 end
